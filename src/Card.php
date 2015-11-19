05e112d31fd08ae72ee9deafb56474a4dcf067f6
jochen <jochen.janssens@tactics.be>
2015-11-19 22:23:02 +0100

    bare layout of a card object, should have written the tests first.

---
+++ src/Card.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace src;
+
+class Card
+{
+    private $kind;
+    private $value;
+
+    /**
+     * Card constructor.
+     * @param string $kind
+     * @param string $value
+     */
+    public function __construct($kind, $value)
+    {
+        $this->kind = $kind;
+        $this->value = $value;
+    }
+
+    // add some guards for kind and value ?
+
+    /**
+     * cards need to be comparable
+     * @param Card $other
+     * @return bool
+     */
+    public function equals(Card $other)
+    {
+        return ($this->kind() === $other->kind() && $this->value() === $other->value());
+    }
+
+    public function kind() { return $this->kind; }
+    public function value() { return $this->value; }
+}
