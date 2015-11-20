749e5631eef4778839ac6e79880adf7996384e19
jochen janssens <jochen.janssens@dragintra.com>
2015-11-20 15:58:20 +0100

    test for equals method

--- test/CardTest.php
+++ test/CardTest.php
@@ -14,4 +14,20 @@ class CardTest extends \PHPUnit_Framework_TestCase
         $card = new Card('H', '2');
         $this->assertSame('H2', $card->kind().$card->value());
     }
+
+    /**
+     * @test
+     */
+    public function cards_are_equal_when_kind_and_value_are_the_same()
+    {
+        $card = new Card('H', '3');
+        $cardWithDifferentKind = new Card('R', '3');
+        $cardWithDifferentValue = new Card('H', '9');
+        $cardWithDifferentKindAndValue = new Card('R', '9');
+
+        $this->assertTrue($card->equals($card));
+        $this->assertFalse($card->equals($cardWithDifferentKind));
+        $this->assertFalse($card->equals($cardWithDifferentValue));
+        $this->assertFalse($card->equals($cardWithDifferentKindAndValue));
+    }
 }
